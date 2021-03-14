657b4acc89f8457d560afea2bd7f0c30b0c435d4
iliyas.mohamed <iliyas.mohamed@gmail.com>
2021-03-14 15:44:30 +0800

    logic to checkin/out,delete devices
    Handled Edge cases as well

--- routes/devices.js
+++ routes/devices.js
@@ -16,6 +16,8 @@ router.get('/', async(req,res) => {
 
 
 //in this module: displaying specific devices available in storage by their ID
+//Edge cases handled in this module:
+    //Indicate if a device has been checked out for more than a week
 router.get('/:id', async(req,res) => {
     try{
            const device = await Device.findById(req.params.id);
@@ -42,14 +44,16 @@ router.get('/:id', async(req,res) => {
 });
 
 //module to add devices to the storage
-//logic to add only 10 devices and the system  prevent adding more
+//Edge cases handled in this module:
+    //logic to add only 10 devices and the system  prevent adding more
+    //Max number of allowed in the garage is 10, the system should prevent adding more.
 router.post('/', async(req,res) => 
 {
     const number = await Device.countDocuments();
         
     if(number < 10)
     {
-        const device = new Device;
+        const device = new Device
         (
             {
                 device: req.body.device,
@@ -59,16 +63,72 @@ router.post('/', async(req,res) =>
         )
         try
         {
-            const a1 =  await device.save();
-            res.json(a1);
+            const a1 =  await device.save() 
+            res.json(a1)
         }catch(err)
         {
-            res.send('Error');
+            res.send('Error')
         }
     }
     else
     {
-        res.send('Max number of devices reached');
+        res.send('Max number of devices reached')
+    }
+})
+
+
+//module to check-in/check-out devices
+//Edge cases handled in this module:
+    //allows checkouts can only be performed between 9:00am - 17:00pm 
+    //validates a device that's already checked out
+    //Each person can only check out one device at a time.
+router.patch('/:id',async(req,res)=> {
+    try{
+        const device = await Device.findById(req.params.id);
+        var date = new Date();
+        const current_hour = date.getHours();
+        
+        var doesUserExist = false;
+        
+        if(req.body.lastCheckedOutBy != null)
+        {
+            doesUserExist = await Device.exists({lastCheckedOutBy: req.body.lastCheckedOutBy},{isCheckedOut:true});
+        }
+        
+        if(device.isCheckedOut == true && req.body.isCheckedOut == true)
+        {res.send('Device already checked out');}
+
+        if(doesUserExist == true)
+        {res.send('Each person can only check out one device at a time.');}
+        if (req.body.isCheckedOut == 'false' || (current_hour >= 9 && current_hour < 17))
+        {
+                    if (req.body.os != null)                 { device.os = req.body.os                                  }   
+                    if (req.body.device != null)             { device.device = req.body.device                          }
+                    if (req.body.feedback != null)           { device.feedback = req.body.feedback                      }
+                    if (req.body.manufacturer != null)       { device.manufacturer = req.body.manufacturer              }        
+                    if (req.body.isCheckedOut != null)       { device.isCheckedOut = req.body.isCheckedOut              }
+                    if (req.body.lastCheckedOutBy != null)   { device.lastCheckedOutBy = req.body.lastCheckedOutBy      }
+                    if (req.body.lastCheckedOutDate != null) { device.lastCheckedOutDate = req.body.lastCheckedOutDate  }
+                    const a1 = await device.save();
+                    res.json(a1);   
+        }else
+        {res.send('You are allowed to check out only between 9 am to 5 pm');
+        }
+    }catch(err){
+        res.send('Error ' + err);
+    }
+})
+
+//module to remove devices from the storage
+router.delete('/:id',async(req,res)=> 
+{
+    try{
+        const device = await Device.findById(req.params.id);
+        const a1 = await device.remove();
+        res.send("Record Deleted......successfully");
+        
+    }catch(err){
+        res.send('Record Not Found');
     }
 })
 
